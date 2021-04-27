a8a903f37c03e10c8a8850234b3e666789bf27fe
Duro Fusic <duro.fusic@picturewealth.com>
2021-04-27 14:19:22 +0200

    components index, remove eslint disable comment

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,6 @@ import Page from './Page';
 import Container from './Container';
 import ErrorMessage from './ErrorMessage';
 import Spinner from './Spinner';
-// eslint-disable-next-line import/no-cycle
 import SinglePost from './SinglePost';
 import Comment from './Comment';
 import Search from './Search';
